756ad72e1931e911ef1e7c120286dbdb406a5a91
li_jinlai <jinlai_li@fpi-inc.com>
2021-07-26 10:47:02 +0800

    i

--- node/event/eventloop.js
+++ node/event/eventloop.js
@@ -21,7 +21,8 @@ Promise.resolve()
     Promise.resolve()
       .then(() => {
         console.log("then1-1");
-        return Promise.resolve(); // x.then().then()
+        return Promise.resolve(); // 两个then的间隔
+        //  return {then(resolve){resolve()}}; // 只会有一个then的间隔
       })
       .then(() => {
         console.log("then1-2");
@@ -50,3 +51,8 @@ Promise.resolve()
 // 4.依次继续清空微任务队列             ---输出then4
 // 微任务队列 [then1-2,then5]
 // 4.依次继续清空微任务队列             ---输出then1-2 then5
+
+// 在一个 pending 状态的 promise 对象（p）的 .then回调里返回一个 promise 对象（ p2），或者任意带有 then 方法的对象，引擎会专门起一个额外的 microtask/job 去执行这个 p2的 then 方法，
+// 同时把 p 的 [[resolve]]和 [[reject]]函数作为参数传过去，虽然 p2 已经 fulfilled 了，
+// 但它能做的也就是把 [[resolve]] 函数立刻放到 microtask 队列里，这样也就过了两个 microtask，这时 p 才会被 fulfill，
+// p 后面的 console.log('内部第二个then')才会被放入队列。
